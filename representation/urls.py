cb236c1c90e8aedaaaad9e13cc130f12835dd1cb
YakovBadyginD <yakov@badygin.ru>
2016-03-16 16:12:07 +0300

    #2 added friends functional

--- representation/urls.py
+++ representation/urls.py
@@ -1,11 +1,14 @@
 
 from django.conf.urls import url
-
-from representation.views import auth, views, profile
+from representation.views import auth, views, profile, friend
 
 urlpatterns = [
+    url(r'^test/', views.test, name='test'),
+
     url(r'^$', views.index, name='index'),
-    url(r'^id(?P<user_id>\d+)', views.user_page, name='userPage'),
-    url(r'^profile/', profile.url_view()),
+    url(r'^id(?P<user_id>\d+)', views.UserPageView.as_view(), name='userPage'),
     url(r'^auth/', auth.url_view()),
+
+    url(r'^profile/', profile.url_view()),
+    url(r'^friends/', friend.url_view()),
 ]
