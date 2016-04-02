fe927d93af84c2a0566599052d845f5d6ed153ee
Yakov Badygin <yakov.badygin@gmail.com>
2016-04-02 15:07:51 +0400

    #16 begin, use class

--- representation/admin.py
+++ representation/admin.py
@@ -3,11 +3,55 @@ from django.contrib import admin
 
 from representation.models import Language, Country, City, UserProfile, FriendRequest, Message, Comment, Post
 
-admin.site.register(Language)
-admin.site.register(City)
-admin.site.register(Country)
-admin.site.register(UserProfile)
-admin.site.register(FriendRequest)
-admin.site.register(Message)
-admin.site.register(Comment)
-admin.site.register(Post)
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'name', 'birthday']
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(FriendRequest)
+class FriendRequestAdmin(admin.ModelAdmin):
+    list_display = ['from_user', 'to_user', 'accepted', 'denied']
+    readonly_fields = list_display + ['message']
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
